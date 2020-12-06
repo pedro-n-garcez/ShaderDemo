e1c27e91b5bc1a463af46deec0f33bc9a7b4fe69
Pedro <pedro.garcez@loop.colum.edu>
2020-12-06 19:20:32 -0300

    Added particle system with mesh renderer
    -Added particle system that uses 3d mesh particles instead of 2d texture particles.
    -Uses custom transparent shader

---
+++ Assets/log.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class log : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Supports Compute Shaders: " + SystemInfo.supportsComputeShaders);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
